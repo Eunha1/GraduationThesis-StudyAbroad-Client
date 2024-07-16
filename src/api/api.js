5f3c0084ec8999516a5b1fa8a27c9aa38715b5b2
Eunha1 <doquangphuc18102001@gmail.com>
2024-07-16 23:18:19 +0700

    fix

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,6 @@ export async function getRequest(url) {
   try {
     let response = await axios.get(
       'https://project.truyentienganh.com' + '/api' + url,
-      //'http://localhost:3001' + '/api' + url
     );
     return response.data;
   } catch (error) {
@@ -16,7 +15,6 @@ export async function postRequest(url, body) {
   try {
     let response = await axios.post(
       'https://project.truyentienganh.com' + '/api' + url,
-      //'http://localhost:3001' + '/api' + url,
       body,
     );
     return response.data;
